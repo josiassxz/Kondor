9197c76a86c25242e0d06ce3773699045319550e
Josias Silva Siqueira <josiassxz@hotmail.com>
2022-10-31 09:59:32 -0300

    initial commit

---
+++ src/Screens/Settings/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Settings = () => {
+    return(
+        <div>
+            <h1>Voce esta na pagina Settings</h1>
+        </div>
+    );
+}
+
+export default Settings
