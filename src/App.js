76ccacea2fc4ef382b6defeb890419c63074ced8
Josias Silva Siqueira <josiassxz@hotmail.com>
2022-11-02 11:02:31 -0300

    router created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import React from 'react';
 import './App.css';
-import Header from "./components/Header";
-import Home from "./Screens/Home";
-import Users from "./Screens/Users";
-import Settings from "./Screens/Settings";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
+import Dashboard from './pages/Dashboard.jsx';
+import About from './pages/About.jsx';
+import Analytics from './pages/Analytics.jsx';
+import Comment from './pages/Comment.jsx';
+import Product from './pages/Product.jsx';
+import ProductList from './pages/ProductList.jsx';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <Header/>
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/users" element={<Users/>}/>
-                <Route path="/settings" element={<Settings/>}></Route>
-            </Routes>
-        </Router>
-    </div>
+    <BrowserRouter>
+      <Sidebar>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/comment" element={<Comment />} />
+          <Route path="/analytics" element={<Analytics />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/productList" element={<ProductList />} />
+        </Routes>
+      </Sidebar>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
